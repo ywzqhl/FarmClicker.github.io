9a6d5fd0d524d5647c6108e96936134f309273dc
ywzqhl <450099136@qq.com>
2024-06-02 10:22:55 +0800

    Update app.js

--- app.js
+++ app.js
@@ -13,3 +13,68 @@ FarmClickerApp.config(function($routeProvider){
     redirectTo: '/'
   });
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+    let currentBalance = 0;
+    let harvestPerClick = 1;
+    let siloStorage = 200;
+    let maxStorage = 200;
+
+    const levels = [
+        {name: "Bronze", balance: 0},
+        {name: "Silver", balance: 5000},
+        {name: "Gold", balance: 25000},
+        {name: "Platinum", balance: 100000},
+        {name: "Diamond", balance: 1000000},
+        {name: "Epic", balance: 2000000},
+        {name: "Legendary", balance: 10000000},
+        {name: "Master", balance: 50000000},
+        {name: "Grandmaster", balance: 100000000}
+    ];
+
+    function updateLevel() {
+        for (let i = levels.length - 1; i >= 0; i--) {
+            if (currentBalance >= levels[i].balance) {
+                harvestPerClick = i + 1;
+                maxStorage = levels[i].balance;
+                document.getElementById("level").innerText = levels[i].name;
+                break;
+            }
+        }
+    }
+
+    function clickHandler() {
+        if (siloStorage < maxStorage) {
+            currentBalance += harvestPerClick;
+            siloStorage += harvestPerClick;
+            updateLevel();
+            document.getElementById("balance").innerText = currentBalance;
+            document.getElementById("siloStorage").innerText = siloStorage;
+            savePlayerData(); // Save the data to MongoDB
+        } else {
+            alert('Silo is full!');
+        }
+    }
+
+    function savePlayerData() {
+        fetch('/save', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                currentBalance: currentBalance,
+                harvestPerClick: harvestPerClick,
+                siloStorage: siloStorage,
+                maxStorage: maxStorage
+            })
+        });
+    }
+
+    document.getElementById("clickButton").addEventListener("click", clickHandler);
+
+    // Initialize game elements
+    document.getElementById("balance").innerText = currentBalance;
+    document.getElementById("siloStorage").innerText = siloStorage;
+    document.getElementById("level").innerText = "Bronze";
+});
