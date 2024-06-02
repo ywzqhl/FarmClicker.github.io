ed48ca462a3ec5a86cc08481fb7fbfe7c42e8e95
ywzqhl <450099136@qq.com>
2024-06-02 13:59:29 +0800

    Update StorageUpgrades.js

--- modules/storage/StorageUpgrades.js
+++ modules/storage/StorageUpgrades.js
@@ -1,1 +1,1 @@
-[{"name":"Silo", "image":"../../assets/img/barn.png", "bought":0, "price":500, "increaseStorageBy":price, "increase":price}]
+[{"name":"Silo", "image":"../../assets/img/barn.png", "bought":0, "price":200, "increaseStorageBy":price, "increase":price}]
