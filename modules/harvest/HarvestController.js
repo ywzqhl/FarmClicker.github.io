5f91331835190f0ade63552657c81e4793964141
ywzqhl <450099136@qq.com>
2024-06-06 23:56:38 +0800

    Add files via upload

--- modules/harvest/HarvestController.js
+++ modules/harvest/HarvestController.js
@@ -1,31 +1,36 @@
-FarmClickerApp.controller('HarvestController', ['$scope', '$http', 'harvest', 'storage', function($scope, $http, harvest, storage){
+// Define a controller named 'HarvestController' to manage the harvesting view
+FarmClickerApp.controller('HarvestController', ['$scope', '$interval', 'harvest', 'storage', function($scope, $interval, harvest, storage){
 
-  // $scope.harvestData = {};
-  //
-  // $http.get('/modules/harvest/HarvestData.js').success(function(data) {
-  //   $scope.harvestData = data;
-  // });
-
+  // Get the path to the harvest button image
   $scope.getHarvestCropButton = function(){
     return harvest.getHarvestCropButton();
   };
 
+  // Get the number of crops harvested
   $scope.getCropsHarvested = function(){
     return harvest.getCropsHarvested();
   };
 
+  // Get the number of crops harvested per second
   $scope.getHarvestPerSec = function(){
     return harvest.getHarvestPerSec();
   };
 
+  // Harvest crops when the button is clicked
   $scope.harvestCrops = function(){
-    // $scope.pulseToggle = !$scope.pulseToggle
-    if (harvest.getCropsHarvested() + harvest.getHarvestPerClick() <= storage.getMaxStorage())
-      harvest.harvestCrops();
-    else {
-      //harvest.addToHarvest(storage.getMaxStorage() - harvest.getCropsHarvested());
+    var harvestAmount = harvest.getHarvestPerClick();
+    if (storage.getCurrentStorage() >= harvestAmount) {
+      storage.decreaseCurrentStorage(harvestAmount);
       harvest.harvestCrops();
     }
   };
+
+  // Automatically refill the current storage every second
+  $interval(function() {
+    storage.autoRefillStorage(harvest.getHarvestPerSec());
+  }, 1000);
+
+  // Update the last online time when the player logs in or re-enters the page
+  storage.updateLastOnlineTime();
 
 }]);
--- modules/harvest/HarvestService.js
+++ modules/harvest/HarvestService.js
@@ -1,50 +1,58 @@
+// Define a service named 'harvest'
 FarmClickerApp.service('harvest', function(){
-  var harvestCropButton = "../../assets/img/oat.png";
-  var cropsHarvested = 0;
-  var harvestPerSec = 0;
-  var harvestPerClick = 1;
+  var harvestCropButton = "../../assets/img/oat.png"; // Path to the harvest crop button image
+  var cropsHarvested = 0; // Number of crops harvested
+  var harvestPerSec = 0; // Crops harvested per second
+  var harvestPerClick = 1; // Crops harvested per click
 
+  // Get the harvest crop button image path
   this.getHarvestCropButton = function(){
     return harvestCropButton;
   };
 
+  // Get the number of crops harvested
   this.getCropsHarvested = function(){
     return cropsHarvested;
   };
 
+  // Deduct crops from the harvested crops count
   this.deductFromHarvest = function(amount){
     cropsHarvested -= amount;
   };
 
+  // Harvest crops, increase crops harvested per click
   this.harvestCrops = function(){
     cropsHarvested += harvestPerClick;
   };
 
+  // Add crops to the harvested crops count
   this.addToHarvest = function(amount){
     cropsHarvested += amount;
   };
 
+  // Get crops harvested per second
   this.getHarvestPerSec = function(){
     return harvestPerSec;
   };
 
+  // Get crops harvested per click
   this.getHarvestPerClick = function(){
     return harvestPerClick;
   };
 
+  // Increase crops harvested per second
   this.increaseHarvestPerSec = function(amount){
     harvestPerSec += amount;
   };
 
+  // Increase crops harvested per click
   this.increaseHarvestPerClick = function(amount){
     harvestPerClick += amount;
   };
 
+  // Check if an upgrade is unavailable
   this.unavailable = function(object){
     if (cropsHarvested < object.price)
       return "unavailable";
   };
-
-
-
 });
