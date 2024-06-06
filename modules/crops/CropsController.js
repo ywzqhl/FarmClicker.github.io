dd789aa7136d0dbe1a26e86572e9da9d1cc62f50
ywzqhl <450099136@qq.com>
2024-06-06 23:56:11 +0800

    Add files via upload

--- modules/crops/CropsController.js
+++ modules/crops/CropsController.js
@@ -1,19 +1,19 @@
 FarmClickerApp.controller('CropsController', ['$scope', '$http', 'productivity', 'harvest', function($scope, $http, productivity, harvest){
 
-  $scope.cropsData = {};
+  $scope.cropsData = {}; // 初始化作用域中的 cropsData 对象
 
+  // 使用 $http 服务从 '/modules/crops/CropsData.js' 获取作物数据
   $http.get('/modules/crops/CropsData.js').success(function(data) {
-    $scope.cropsData = data;
+    $scope.cropsData = data; // 成功获取数据后，将其赋值给 $scope.cropsData
   });
 
+  // 定义一个函数，当用户点击某个作物时调用
   $scope.clickCrop = function(crop){
-    if (harvest.getCropsHarvested() >= crop.price) {
-            harvest.deductFromHarvest(crop.price);
-            harvest.increaseHarvestPerClick(crop.perClick);
-
-            crop.bought = true;
-        }
+    if (harvest.getCropsHarvested() >= crop.price) { // 检查收割的作物数量是否大于或等于作物的价格
+      harvest.deductFromHarvest(crop.price); // 从收割的作物数量中扣除作物的价格
+      harvest.increaseHarvestPerClick(crop.perClick); // 增加每次点击的收割量
+      crop.bought = true; // 将作物的 'bought' 属性设置为 true，表示已购买
+    }
   };
-
 
 }]);
--- modules/crops/CropsData.js
+++ modules/crops/CropsData.js
@@ -1,5 +1,24 @@
 [
-  {"name":"Wheat", "image":"../../assets/img/wheat.png", "bought":false, "price":100, "perClick":2},
-  {"name":"Corn", "image":"../../assets/img/corn.png", "bought":false, "price":500, "perClick":10},
-  {"name":"Carrots", "image":"../../assets/img/carrot.png", "bought":false, "price":5000, "perClick":50}
+  {
+    "name": "Wheat",        // 作物的名称
+    "image": "../../assets/img/wheat.png", // 作物图片的相对路径
+    "bought": false,        // 是否已购买，初始值为 false
+    "price": 100,           // 作物的价格
+    "perClick": 2           // 每次点击增加的收割量
+  },
+  {
+    "name": "Corn",         // 作物的名称
+    "image": "../../assets/img/corn.png",  // 作物图片的相对路径
+    "bought": false,        // 是否已购买，初始值为 false
+    "price": 500,           // 作物的价格
+    "perClick": 10          // 每次点击增加的收割量
+  },
+  {
+    "name": "Carrots",      // 作物的名称
+    "image": "../../assets/img/carrot.png", // 作物图片的相对路径
+    "bought": false,        // 是否已购买，初始值为 false
+    "price": 5000,          // 作物的价格
+    "perClick": 50          // 每次点击增加的收割量
+  }
 ]
+
