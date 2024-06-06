4a1fff3f74a46efbf2981648e950db69be4abe90
ywzqhl <450099136@qq.com>
2024-06-06 23:58:36 +0800

    Add files via upload

--- modules/productivity/ProductivityController.js
+++ modules/productivity/ProductivityController.js
@@ -1,22 +1,32 @@
+// 定义一个名为 'ProductivityController' 的控制器，并注入 '$scope'、'$http'、'productivity' 和 'harvest' 作为依赖
 FarmClickerApp.controller('ProductivityController', ['$scope', '$http', 'productivity', 'harvest', function($scope, $http, productivity, harvest){
 
+  // 初始化作用域中的 productivityUpgrades 对象
   $scope.productivityUpgrades = {};
 
+  // 使用 $http 服务从 '/modules/productivity/ProductivityUpgrades.js' 获取生产力升级数据
   $http.get('/modules/productivity/ProductivityUpgrades.js').success(function(data) {
+    // 成功获取数据后，将其赋值给 $scope.productivityUpgrades
     $scope.productivityUpgrades = data;
   });
 
+  // 定义一个函数，当用户点击某个升级时调用
   $scope.clickUpgrade = function(upgrade){
+    // 检查收割的作物数量是否大于或等于升级的价格
     if (harvest.getCropsHarvested() >= upgrade.price) {
-            harvest.deductFromHarvest(upgrade.price);
-            harvest.increaseHarvestPerSec(upgrade.persecond);
+      // 如果条件满足，从收割的作物数量中扣除升级的价格
+      harvest.deductFromHarvest(upgrade.price);
+      // 增加每秒收割的作物数量
+      harvest.increaseHarvestPerSec(upgrade.persecond);
 
-            upgrade.bought += 1;
-            upgrade.price += Math.floor(upgrade.price * upgrade.increase)
-        }
+      // 增加已购买的升级数量
+      upgrade.bought += 1;
+      // 增加升级的价格，按一定比例增加
+      upgrade.price += Math.floor(upgrade.price * upgrade.increase);
+    }
   };
 
-  // //Returns the class to display if the upgrade is not available
+  // // 返回显示不可用升级的类（已注释掉）
   // $scope.unavailable = function(upgrade){
   //   return harvest.unavailable(upgrade);
   // };
--- modules/productivity/ProductivityService.js
+++ modules/productivity/ProductivityService.js
@@ -1,16 +1,18 @@
 FarmClickerApp.service('productivity', function(){
 
-  // //Return the number of workers, or horses, or tractors, etc
+  // // 返回指定升级的数量，例如工人、马匹、拖拉机等
   // this.getUpgrade = function(upgradeName){
-  //   return upgrades[upgradeName];
+  //   return upgrades[upgradeName]; // 返回指定升级的数量
   // };
   //
+  // // 增加指定升级的数量
   // this.addToUpgrades = function(upgradeName, amount){
-  //   upgrades[upgradeName] += amount;
+  //   upgrades[upgradeName] += amount; // 增加指定升级的数量
   // };
   //
+  // // 获取雇用工人按钮的状态
   // this.getHireWorkerButton = function(){
-  //   return hireWorkerButton;
+  //   return hireWorkerButton; // 返回雇用工人按钮的状态
   // };
 
 });
