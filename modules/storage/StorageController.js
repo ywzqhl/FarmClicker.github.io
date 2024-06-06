cbc2f783e6bc28671753b5c674e9a1a7201eda4f
ywzqhl <450099136@qq.com>
2024-06-07 00:00:40 +0800

    Add files via upload

--- modules/storage/StorageController.js
+++ modules/storage/StorageController.js
@@ -1,28 +1,38 @@
+// Define a controller named 'StorageController' to manage the storage view
 FarmClickerApp.controller('StorageController', ['$scope', '$http', 'harvest', 'storage', function($scope, $http, harvest, storage){
 
+  // Initialize the scope variable for storage upgrades
   $scope.storageUpgrades = {};
 
+  // Fetch the storage upgrades data from the specified file
   $http.get('/modules/storage/StorageUpgrades.js').success(function(data) {
     $scope.storageUpgrades = data;
   });
 
+  // Get the maximum storage capacity
   $scope.getMaxStorage = function(){
     return storage.getMaxStorage();
   };
 
+  // Get the current storage usage
+  $scope.getCurrentStorage = function(){
+    return storage.getCurrentStorage();
+  };
+
+  // Increase the maximum storage capacity
   $scope.increaseMaxStorage = function(amount){
     storage.increaseMaxStorage(amount);
   };
 
+  // Handle the click event for purchasing a storage upgrade
   $scope.clickSilo = function(upgrade){
     if (harvest.getCropsHarvested() >= upgrade.price) {
-            harvest.deductFromHarvest(upgrade.price);
-            storage.increaseMaxStorage(upgrade.increaseStorageBy);
-
-            upgrade.bought += 1;
-            upgrade.price += upgrade.increase;
-        }
+      harvest.deductFromHarvest(upgrade.price);
+      storage.increaseMaxStorage(upgrade.increaseStorageBy);
+      upgrade.bought += 1;
+      upgrade.price += upgrade.increase;
+    }
   };
 
-
 }]);
+
--- modules/storage/StorageService.js
+++ modules/storage/StorageService.js
@@ -1,12 +1,47 @@
+// Define a service named 'storage' to manage storage logic
 FarmClickerApp.service('storage', function(){
-  var maxStorage = 200;
+  var maxStorage = 200; // Maximum storage capacity
+  var currentStorage = maxStorage; // Current storage usage initialized to maxStorage
+  var lastOnlineTime = new Date(); // Track the last time the player was online
 
+  // Get the maximum storage capacity
   this.getMaxStorage = function(){
     return maxStorage;
   };
 
+  // Increase the maximum storage capacity
   this.increaseMaxStorage = function(amount){
     maxStorage += amount;
   };
 
+  // Get the current storage usage
+  this.getCurrentStorage = function(){
+    return currentStorage;
+  };
+
+  // Increase the current storage usage
+  this.increaseCurrentStorage = function(amount){
+    currentStorage += amount;
+    if (currentStorage > maxStorage) currentStorage = maxStorage; // Ensure storage doesn't exceed maxStorage
+  };
+
+  // Decrease the current storage usage
+  this.decreaseCurrentStorage = function(amount){
+    currentStorage -= amount;
+    if (currentStorage < 0) currentStorage = 0; // Ensure storage doesn't go negative
+  };
+
+  // Auto refill current storage
+  this.autoRefillStorage = function(amount){
+    var currentTime = new Date();
+    var timeDifference = (currentTime - lastOnlineTime) / (1000 * 60 * 60); // Time difference in hours
+    if (timeDifference < 3) {
+      this.increaseCurrentStorage(amount);
+    }
+  };
+
+  // Update last online time to the current time
+  this.updateLastOnlineTime = function(){
+    lastOnlineTime = new Date();
+  };
 });
