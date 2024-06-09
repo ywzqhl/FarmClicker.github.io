c2bef3105839cb5e0ba2445fa3af12ef07b91641
ywzqhl <450099136@qq.com>
2024-06-09 16:58:44 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
+// Create an AngularJS module named 'FarmClickerApp' with 'ngRoute' as a dependency
 var FarmClickerApp = angular.module('FarmClickerApp', ['ngRoute']);
-//TODO: routing
+
+// Configure routes
 FarmClickerApp.config(function($routeProvider){
   $routeProvider
+  // When the URL is '/', use 'ApplicationController' and load 'app.html' template
   .when('/', {
     controller: 'ApplicationController',
     templateUrl: 'modules/application/app.html'
   })
+  // When the URL is '/about', load 'about.html' template
   .when('/about', {
     templateUrl: 'modules/about/about.html'
   })
+  // For any other URL, redirect to '/'
   .otherwise({
     redirectTo: '/'
   });
